4de64ae62ca08498afbf4b8c59c884b7585945f1
Zé Elias <elias.jpaiva@gmail.com>
2022-06-07 11:44:03 +0200

    adding logic to the program

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const textOne = document.getElementById('invalid-number-text-one');
+const textTwo = document.getElementById('invalid-number-text-two');
+const box = document.getElementById('user-input');
+
+box.addEventListener('input', function () {
+  if (box.value !=	)
+});
