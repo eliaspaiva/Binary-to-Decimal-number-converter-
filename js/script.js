6c6f7f8700a8bf02daab13d1b9200cab7f668f9c
Zé Elias <elias.jpaiva@gmail.com>
2022-06-08 19:23:23 +0200

    improving logic to check input value

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
-const numberOne = 1;
-const numberZero = 0;
-const textOne = document.getElementById('invalid-number-text-one');
-const textTwo = document.getElementById('invalid-number-text-two');
-const box = document.getElementById('user-input');
+// Variables
+const numberOne = "1";
+const numberZero = "0";
 
-box.addEventListener('input', function(e) {
+// Dom Elements
+const invalidNumberTextOne = document.getElementById('invalid-number-text-one');
+const invalidNumberTextTwo = document.getElementById('invalid-number-text-two');
+const inputBox = document.getElementById('user-input');
+
+// Check input value
+inputBox.addEventListener('input', function(e) {
     e.preventDefault();
-    const inputValue = box.value;
+    const inputValue = inputBox.value;
     const checkNumber = [];
     checkNumber.push(inputValue);
     const commaSeperated = checkNumber.join(',')
     const inputValueInArray = commaSeperated.split('');
+    console.log(inputValueInArray);
+    for (let i = 0; i < inputValueInArray.length; i++) {
+      if (
+        inputValueInArray[i] != numberOne &&
+        inputValueInArray[i] != numberZero
+      ) {
+        invalidNumberTextOne.innerHTML = 'You entered a non-binary digit (please enter only 0 or 1)'
+        invalidNumberTextTwo.innerHTML =
+        '🤷Waiting for a valid binary number...';
+      }
+    }
 
-    const result = inputValueInArray.filter(numbers => )
     
 });
 
