d3463a84508f81117f4d2cdcf0928fe2774a9abb
Zé Elias <elias.jpaiva@gmail.com>
2022-06-08 18:49:22 +0200

    creating logic to check items in array

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
+const numberOne = 1;
+const numberZero = 0;
 const textOne = document.getElementById('invalid-number-text-one');
 const textTwo = document.getElementById('invalid-number-text-two');
 const box = document.getElementById('user-input');
 
-box.addEventListener('input', function () {
-  if (box.value !=	)
+box.addEventListener('input', function(e) {
+    e.preventDefault();
+    const inputValue = box.value;
+    const checkNumber = [];
+    checkNumber.push(inputValue);
+    const commaSeperated = checkNumber.join(',')
+    const inputValueInArray = commaSeperated.split('');
+
+    const result = inputValueInArray.filter(numbers => )
+    
 });
+
