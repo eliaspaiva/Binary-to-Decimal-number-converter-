f64b8201233da42aa5cd38e7602ac06c50012d29
Zé Elias <elias.jpaiva@gmail.com>
2022-06-09 10:36:41 +0200

    added eslint

--- js/script.js
+++ js/script.js
@@ -1,32 +1,33 @@
 // Variables
-const numberOne = "1";
-const numberZero = "0";
+const numberOne = '1';
+const numberZero = '0';
+// const wrongNumbers = ['2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Dom Elements
 const invalidNumberTextOne = document.getElementById('invalid-number-text-one');
 const invalidNumberTextTwo = document.getElementById('invalid-number-text-two');
 const inputBox = document.getElementById('user-input');
 
-// Check input value
-inputBox.addEventListener('input', function(e) {
-    e.preventDefault();
-    const inputValue = inputBox.value;
-    const checkNumber = [];
-    checkNumber.push(inputValue);
-    const commaSeperated = checkNumber.join(',')
-    const inputValueInArray = commaSeperated.split('');
-    console.log(inputValueInArray);
-    for (let i = 0; i < inputValueInArray.length; i++) {
-      if (
-        inputValueInArray[i] != numberOne &&
-        inputValueInArray[i] != numberZero
-      ) {
-        invalidNumberTextOne.innerHTML = 'You entered a non-binary digit (please enter only 0 or 1)'
-        invalidNumberTextTwo.innerHTML =
-        '🤷Waiting for a valid binary number...';
-      }
+// Functions
+inputValidation = () => {
+  const checkNumber = [];
+  const inputValue = inputBox.value;
+  checkNumber.push(inputValue);
+  const commaSeperated = checkNumber.join(',');
+  const inputValueInArray = commaSeperated.split('');
+  for (let i = 0; i < inputValueInArray.length; i++) {
+    if (
+      inputValueInArray[i] != numberOne &&
+      inputValueInArray[i] != numberZero
+    ) {
+      invalidNumberTextOne.innerHTML =
+        'You entered a non-binary digit (please enter only 0 or 1)';
+      invalidNumberTextTwo.innerHTML = '🤷Waiting for a valid binary number...';
     }
+  }
+};
 
-    
-});
+// Event Listeners
+inputBox.addEventListener('input', inputValidation);
+
 
