ca972b889efccf6b727d4c668cb19dfbef323c42
Zé Elias <elias.jpaiva@gmail.com>
2022-06-15 14:00:47 +0200

    i am back

--- js/script.js
+++ js/script.js
@@ -1,42 +1,38 @@
 // Variables
 const numberOne = '1';
 const numberZero = '0';
-// const wrongNumbers = ['2', '3', '4', '5', '6', '7', '8', '9'];
+const wrongNumbers = ['2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Dom Elements
 const invalidNumberTextOne = document.getElementById('invalid-number-text-one');
 const invalidNumberTextTwo = document.getElementById('invalid-number-text-two');
 const inputBox = document.getElementById('user-input');
 
-
-// Functions
-inputValidation = () => {
-  const checkNumber = [];
+// Check input value
+inputBox.addEventListener('input', function(e) {
+  e.preventDefault();
   const inputValue = inputBox.value;
+  const checkNumber = [];
   checkNumber.push(inputValue);
   const commaSeperated = checkNumber.join(',');
   const inputValueInArray = commaSeperated.split('');
-  console.log(inputValueInArray);
   for (let i = 0; i < inputValueInArray.length; i++) {
     if (
-      inputValueInArray[i] !== numberOne &&
-      inputValueInArray[i] !== numberZero
-    ) {
-      invalidNumberTextOne.innerHTML =
-        'You entered a non-binary digit (please enter only 0 or 1)';
-      invalidNumberTextTwo.innerHTML = '🤷Waiting for a valid binary number...';
-    } else if (
-      inputValueInArray[i] == numberOne ||
-      inputValueInArray[i] == numberZero
+      inputValueInArray[i] === numberOne &&
+      inputValueInArray[i] === numberZero
     ) {
       invalidNumberTextOne.innerHTML = '';
       invalidNumberTextTwo.innerHTML = '';
+      console.log('primeiro');
+    }
+    for (let j = 0; j < wrongNumbers.length; j++) {
+      if (inputValueInArray[i] === wrongNumbers[j]) {
+        invalidNumberTextOne.innerHTML =
+          'You entered a non-binary digit (please enter only 0 or 1)';
+        invalidNumberTextTwo.innerHTML =
+          '🤷Waiting for a valid binary number...';
+        console.log('segundo');
+      }
     }
   }
-};
-
-// Event Listeners
-inputBox.addEventListener('input', inputValidation);
-console.log(inputBox.value.length);
-
-// ||
+});
