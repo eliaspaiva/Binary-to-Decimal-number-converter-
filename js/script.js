8490a332a47971c6dcb6baceea330babd573baa5
Zé Elias <elias.jpaiva@gmail.com>
2022-06-16 15:43:46 +0200

    input only accepts 0 and 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // Variables
-const numberOne = '1';
-const numberZero = '0';
+const binaryNumbers = ['0', '1'];
 const wrongNumbers = ['2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Dom Elements
@@ -16,22 +15,31 @@ inputBox.addEventListener('input', function(e) {
   checkNumber.push(inputValue);
   const commaSeperated = checkNumber.join(',');
   const inputValueInArray = commaSeperated.split('');
+  const lastElement = inputValueInArray.slice(-1);
+  const secondToLast = inputValueInArray.slice(-2);
   for (let i = 0; i < inputValueInArray.length; i++) {
-    if (
-      inputValueInArray[i] === numberOne ||
-      inputValueInArray[i] === numberZero
-    ) {
-      invalidNumberTextOne.innerHTML = '';
-      invalidNumberTextTwo.innerHTML = '';
-      console.log('primeiro');
-    }
     for (let j = 0; j < wrongNumbers.length; j++) {
-      if (inputValueInArray[i] === wrongNumbers[j]) {
-        invalidNumberTextOne.innerHTML =
-          'You entered a non-binary digit (please enter only 0 or 1)';
-        invalidNumberTextTwo.innerHTML =
-          '🤷Waiting for a valid binary number...';
-        console.log('segundo');
+      for (let x = 0; x < binaryNumbers.length; x++) {
+        if (inputValueInArray[i] === binaryNumbers[x]) {
+          invalidNumberTextOne.innerHTML = '';
+          invalidNumberTextTwo.innerHTML = '';
+          console.log('primeiro');
+        } else if (
+          inputValueInArray[i] === wrongNumbers[j]
+        ) {
+          invalidNumberTextOne.innerHTML =
+            'You entered a non-binary digit (please enter only 0 or 1)';
+          invalidNumberTextTwo.innerHTML =
+            '🤷Waiting for a valid binary number...';
+          console.log('segundo');
+        } else if (
+          inputValueInArray[i] === binaryNumbers[x] &&
+          inputValueInArray[i] === wrongNumbers[j]
+        ) {
+          invalidNumberTextOne.innerHTML = '';
+          invalidNumberTextTwo.innerHTML = '';
+          console.log('terceiro');
+        }
       }
     }
   }
