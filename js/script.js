7ae6103ad77beff4822f41115166e184f423ea76
Zé Elias <elias.jpaiva@gmail.com>
2022-06-21 10:05:42 +0200

    project completed, but without css

--- js/script.js
+++ js/script.js
@@ -1,46 +1,10 @@
-// Variables
-const binaryNumbers = ['0', '1'];
-const wrongNumbers = ['2', '3', '4', '5', '6', '7', '8', '9'];
-
 // Dom Elements
-const invalidNumberTextOne = document.getElementById('invalid-number-text-one');
-const invalidNumberTextTwo = document.getElementById('invalid-number-text-two');
-const inputBox = document.getElementById('user-input');
+const inputValue = document.getElementById('user-input');
+const decimalConversion = document.getElementById('display-decimal-number');
 
 // Check input value
-inputBox.addEventListener('input', function(e) {
-  e.preventDefault();
-  const inputValue = inputBox.value;
-  const checkNumber = [];
-  checkNumber.push(inputValue);
-  const commaSeperated = checkNumber.join(',');
-  const inputValueInArray = commaSeperated.split('');
-  const lastElement = inputValueInArray.slice(-1);
-  const secondToLast = inputValueInArray.slice(-2);
-  for (let i = 0; i < inputValueInArray.length; i++) {
-    for (let j = 0; j < wrongNumbers.length; j++) {
-      for (let x = 0; x < binaryNumbers.length; x++) {
-        if (inputValueInArray[i] === binaryNumbers[x]) {
-          invalidNumberTextOne.innerHTML = '';
-          invalidNumberTextTwo.innerHTML = '';
-          console.log('primeiro');
-        } else if (
-          inputValueInArray[i] === wrongNumbers[j]
-        ) {
-          invalidNumberTextOne.innerHTML =
-            'You entered a non-binary digit (please enter only 0 or 1)';
-          invalidNumberTextTwo.innerHTML =
-            '🤷Waiting for a valid binary number...';
-          console.log('segundo');
-        } else if (
-          inputValueInArray[i] === binaryNumbers[x] &&
-          inputValueInArray[i] === wrongNumbers[j]
-        ) {
-          invalidNumberTextOne.innerHTML = '';
-          invalidNumberTextTwo.innerHTML = '';
-          console.log('terceiro');
-        }
-      }
-    }
-  }
+inputValue.addEventListener('input', () => {
+  const numberInputed = inputValue.value;
+  const binaryNumber = parseInt(numberInputed, 2);
+  decimalConversion.innerHTML = `The decimal number is: ${binaryNumber}`;
 });
